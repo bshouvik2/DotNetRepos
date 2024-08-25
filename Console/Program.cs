7490a96aa0fb0c85fc436e4da29ab27d86fc2209
ricky <bshouvik2@hotmail.com>
2024-08-25 12:06:08 +0530

    console added

---
+++ Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("enter your name:");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("my name is" + name);
+            Console.WriteLine("enter frst no");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second no");
+            double b = Convert.ToDouble(Console.ReadLine());
+            int sum = Convert.ToInt32(a + b);
+            Console.WriteLine("sum of name + a + b is " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
